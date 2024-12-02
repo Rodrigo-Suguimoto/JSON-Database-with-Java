4807d7f9a23d9155dfae2f7e164d8c22b9dc57a6
Rodrigo Suguimoto <rodrigo.suguimoto@10903-MB.local>
2024-12-02 07:11:55 -0300

    Trying to refactor code to maintain the main method in the Server

--- JSON Database with Java/task/src/client/Main.java
+++ JSON Database with Java/task/src/client/Main.java
@@ -5,14 +5,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        server.Main server = new server.Main();
 
         String input;
         do {
             input = scanner.nextLine();
             String[] parts = input.split(" ");
             String command = parts[0];
-        } while (!input.equals("exit"));
 
+            switch (command) {
+                case "set":
+                    if (parts.length >= 3) {
+                        int index = Integer.parseInt(parts[1]);
+                        String text = "";
+
+                        for (int i = 2; i < parts.length; i++) {
+                            text += parts[i] + " ";
+                        }
+
+                        text = text.trim(); // Remove white space
+                        String serverResponse = server.Main.set(index, text);
+
+                        System.out.println(serverResponse);
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                    break;
+                case "get":
+                    if (parts.length == 2) {
+                        int index = Integer.parseInt(parts[1]);
+                        System.out.println(server.Main.get(index));
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                    break;
+                case "delete":
+                    if (parts.length == 2) {
+                        int index = Integer.parseInt(parts[1]);
+                        System.out.println(server.Main.delete(index));
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("ERROR");
+                    break;
+            }
+        } while (!input.equals("exit"));
     }
 }
--- JSON Database with Java/task/src/server/Main.java
+++ JSON Database with Java/task/src/server/Main.java
@@ -1,30 +1,60 @@
 package server;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     private static final int DATABASE_SIZE = 1000;
-    private final String[] database = new String[DATABASE_SIZE];
+    private static final String[] database = new String[DATABASE_SIZE];
 
-    public Main() {
-        Arrays.fill(this.database, "");
+    public static void main(String[] args) {
+        Arrays.fill(database, "");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("exit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ", 3);
+            String command = parts[0];
+
+            switch (command) {
+                case "set":
+                    int index = Integer.parseInt(parts[1]);
+                    String text = parts[2];
+                    set(index, text);
+                    break;
+                case "get":
+                    index = Integer.parseInt(parts[1]);
+                    System.out.println(server.Main.get(index));
+                    break;
+                case "delete":
+                    index = Integer.parseInt(parts[1]);
+                    System.out.println(delete(index));
+                    break;
+            }
+        }
     }
 
-    public String set(int index, String text) {
+    public static String set(int index, String text) {
         if (index < 1 || index > DATABASE_SIZE) {
             return "ERROR";
         }
 
-        this.database[index - 1] = text; // Adjust for 0-based indexing
+        database[index - 1] = text; // Adjust for 0-based indexing
         return "OK";
     }
 
-    public String get(int index) {
+    public static String get(int index) {
         if (index < 1 || index > DATABASE_SIZE) {
             return "ERROR";
         }
 
-        String textRetrieved = this.database[index - 1];
+        String textRetrieved = database[index - 1];
         if (textRetrieved.equals("")) {
             return "ERROR";
         } else {
@@ -32,12 +62,12 @@ public class Main {
         }
     }
 
-    public String delete(int index) {
+    public static String delete(int index) {
         if (index < 1 || index > DATABASE_SIZE) {
             return "ERROR";
         }
 
-        this.database[index - 1] = "";
+        database[index - 1] = "";
         return "OK";
     }
 }
